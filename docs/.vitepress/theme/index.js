ee44bb5f3e02f3583da524ffab13e7c7839bdf0e
Marcin Kondrat <breftejk@gmail.com>
2022-11-09 13:04:34 +0100

    adsense

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,15 +2,19 @@ import { inBrowser } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { isInvalidRoute, redirect } from './plugins/redirect'
 
+import GoogleAdsense from './components/GoogleAdsense.vue'
+
 const theme = {
     ...DefaultTheme,
     enhanceApp({ app, router }) {
-        DefaultTheme.enhanceApp({ app, router })
+        app.component('GoogleAdsense', GoogleAdsense)
         if (inBrowser) {
             if (isInvalidRoute()) {
                 redirect()
             }
         }
+
+        DefaultTheme.enhanceApp({ app, router })
     },
 }
 
